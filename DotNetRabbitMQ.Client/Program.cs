3853837797122f24a7d7b5dd949d828b6c19a6f9
Chung Hak Ngor <chunghakngor@gmail.com>
2021-11-17 22:55:34 +1300

    refactor client code

--- DotNetRabbitMQ.Client/Program.cs
+++ DotNetRabbitMQ.Client/Program.cs
@@ -1,23 +1,13 @@
-﻿using System.Text.Json;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
+﻿Console.WriteLine("Starting RabbitMQ Client!");
 
-Console.WriteLine("Starting RabbitMQ Client!");
+const string _hostName = "localhost";
+const short _port = 5672;
+const string _queueName = "testQueue";
 
-ConnectionFactory factory = new() {HostName = "localhost", Port = 5672};
+ConnectionFactory factory = new() { HostName = _hostName, Port = _port };
 using IConnection connection = factory.CreateConnection();
 using IModel channel = connection.CreateModel();
 
-string queueName = "testQueue";
-channel.QueueDeclare(queueName, false, false, false, null);
-
-EventingBasicConsumer consumer = new(channel);
-
-consumer.Received += (model, eventArgs) => {
-    string? body = JsonSerializer.Deserialize<string>(eventArgs.Body.ToArray());
-    Console.WriteLine($"Received Message from {queueName}: {body}");
-};
-
-channel.BasicConsume(queueName, true, consumer);
+Subscriber.Listen<string>(channel, _queueName);
 
 Console.ReadLine();
---
+++ DotNetRabbitMQ.Client/Subscriber.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client.Events;
+
+public static class Subscriber
+{
+	public static void Listen<T>(IModel channel, string queueName)
+	{
+		channel.QueueDeclare(queueName, false, false, false, null);
+		EventingBasicConsumer consumer = new(channel);
+		consumer.Received += (model, eventArgs) => {
+			T? body = JsonSerializer.Deserialize<T>(eventArgs.Body.ToArray());
+			Console.WriteLine($"Received Message from {queueName}: {body}");
+		};
+		channel.BasicConsume(queueName, true, consumer);
+	}
+}
