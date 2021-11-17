233435c7e63e10091697e9a4a5880a4a021a71da
Chung Hak Ngor <chunghakngor@gmail.com>
2021-11-17 23:30:14 +1300

    added test client to send message

---
+++ DotNetRabbitMQ.Test/Program.cs
@@ -0,0 +1,11 @@
+﻿using RabbitMQ.Client;
+
+const string _hostName = "localhost";
+const int _port = 5672;
+const string _queueName = "testQueue";
+
+ConnectionFactory factory = new() { HostName = _hostName, Port = _port };
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+TestClient.Start(channel, _queueName);
---
+++ DotNetRabbitMQ.Test/TestClient.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using DotNetRabbitMQ.Publisher.DependencyInjection;
+using RabbitMQ.Client;
+
+public static class TestClient
+{
+	/// <summary>
+	/// Send a test message to the queue after a delay
+	/// </summary>
+	/// <param name="channel">The IModel for the RabbitMQ Channel</param>
+	/// <param name="queueName">The name of the queue</param>
+	/// <param name="delay">The amount of delay between each message (Default = 1000ms)</param>
+	public static void Start(IModel channel, string queueName, int delay = 1000)
+	{
+		Publisher publisher = new();
+
+		int counter = 0;
+		while (true) {
+			byte[] messageBody = JsonSerializer.SerializeToUtf8Bytes($"Sample test message #{counter++} from DotnetRabbitMQ.Test");
+			publisher.SendMessage(channel, queueName, messageBody);
+			Thread.Sleep(delay);
+		}
+	}
+}
+
